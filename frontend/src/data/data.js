153e8577cfe97d045965cc6de10510024bf93549
Alex76456 <70705720+Alex76456@users.noreply.github.com>
2021-04-26 08:51:54 +0300

    feat: добавить кнопку показать еще

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -1,7 +1,9 @@
 export const testUser = {
 	_id: '607bd8d5b078180217ddc70f',
 	name: 'Васян',
-	link: '@vasya'
+	link: '@vasya',
+	avatar:
+		'https://sun9-20.userapi.com/impf/6fMvMYimCizkrkuu3J2kxmhSNFBG527eDdpxtw/5_W3AOyaePo.jpg?size=960x651&quality=96&proxy=1&sign=60ccc3506cd000b53c36cfa635035129&c_uniq_tag=Rt_H9HOb4LdM66_3x4uLEo40O4LL3zUyMzqdxDo_hnw&type=album'
 };
 
 export const murms = [
@@ -28,5 +30,53 @@ export const murms = [
 		link: '@pryanik',
 		owner: '607c31d9d8f37c08e0157308',
 		text: 'Ну и третий мурм, хосбади!'
+	},
+	{
+		likes: [ '607bd8d5b078180217ddc70f', '6081f424d8f37c08e015731e' ],
+		_id: '607bd90db078180217ddc111',
+		name: 'Васян',
+		link: '@vasya',
+		owner: '607bd8d5b078180217ddc70f',
+		text: 'Это четвертый мурм, детка!'
+	},
+	{
+		likes: [],
+		_id: '607bd91bb078180217ddc222',
+		name: 'Васян',
+		link: '@vasya',
+		owner: '607bd8d5b078180217ddc70f',
+		text: 'А это пятый мурм!'
+	},
+	{
+		likes: [ '607c31d9d8f37c08e0157308' ],
+		_id: '607d7f39d8f37c08e0157333',
+		name: 'Толян',
+		link: '@pryanik',
+		owner: '607c31d9d8f37c08e0157308',
+		text: 'Ну и шестой мурм, хосбади!'
+	},
+	{
+		likes: [ '607bd8d5b078180217ddc70f', '6081f424d8f37c08e015731e' ],
+		_id: '607bd90db078180217ddc444',
+		name: 'Васян',
+		link: '@vasya',
+		owner: '607bd8d5b078180217ddc70f',
+		text: 'Это седьмой мурм, детка!'
+	},
+	{
+		likes: [],
+		_id: '607bd91bb078180217ddc555',
+		name: 'Васян',
+		link: '@vasya',
+		owner: '607bd8d5b078180217ddc70f',
+		text: 'А это восьмой мурм!'
+	},
+	{
+		likes: [ '607c31d9d8f37c08e0157666' ],
+		_id: '607d7f39d8f37c08e0157666',
+		name: 'Толян',
+		link: '@pryanik',
+		owner: '607c31d9d8f37c08e0157308',
+		text: 'Ну и девятый мурм, хосбади!'
 	}
-];
+].reverse();
