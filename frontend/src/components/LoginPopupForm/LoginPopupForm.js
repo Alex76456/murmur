6446de4015c3c1ee8f031b2a8387122bea738a50
Vladislav Kozhevatov <envydef@gmail.com>
2021-04-27 20:00:02 +0300

    feat: add validation for popups

--- frontend/src/components/LoginPopupForm/LoginPopupForm.js
+++ frontend/src/components/LoginPopupForm/LoginPopupForm.js
@@ -1,38 +1,49 @@
 import PopupWithForm from '../PopupWithForm/PopupWithForm';
 import React from 'react';
+import { useFormWithValidation } from '../../utils/Validation/Validation'
+
 
 function LoginPopupForm({ isOpened, onClose }) {
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
+    // const [email, setEmail] = React.useState('');
+    // const [password, setPassword] = React.useState('');
 
-    function handlePassword(e) {
-        setPassword(e.target.value);
-    }
-    function handleEmail(e) {
-        setEmail(e.target.value);
-    }
-    function handleSubmit(e) {
+    // function handlePassword(e) {
+    //     setPassword(e.target.value);
+    // }
+    // function handleEmail(e) {
+    //     setEmail(e.target.value);
+    // }
+    // function handleSubmit(e) {
+    //     e.preventDefault();
+    //     console.log('SUBMIT')
+    //     // onRegistration({
+    //     //     profileNameInput: name,
+    //     //     profilePassword: password,
+    //     //     profileEmail: email,
+    //     // });
+
+    // }
+    const { value, handleChange, errors, isValid } = useFormWithValidation({});
+
+    const onSubmit = (e) => {
         e.preventDefault();
-        console.log('SUBMIT')
-        // onRegistration({
-        //     profileNameInput: name,
-        //     profilePassword: password,
-        //     profileEmail: email,
-        // });
-
+        if (isValid) {
+            console.log('Success');
+        }
     }
+
     return (
         <>
-            <PopupWithForm onSubmit={handleSubmit} button='Сохранить' name='login' title='Авторизация'
+            <PopupWithForm onSubmit={onSubmit} button='Сохранить' name='login' title='Авторизация'
                 isOpened={isOpened} onClose={onClose}>
-                <input value={email || ''} onChange={handleEmail} placeholder="Почта"
-                    id='login-email-input' name='profileEmailInput' className="popup__input-item popup__input-item_el_email" type="email"
+                <input onChange={handleChange} placeholder="Почта"
+                    id='login-email-input' name='email' className="popup__input-item popup__input-item_el_email" type="email"
                     required minLength="2" maxLength="40" />
-                <span id='email-input-error' className="error"></span>
-                <input value={password || ''} onChange={handlePassword} placeholder="Пароль"
-                    id='login-password-input' name='profilePasswordInput' className="popup__input-item popup__input-item_el_password" type="password"
+                <span className="input-error">{errors && errors['email'] !== '' && errors['email']}</span>
+                <input onChange={handleChange} placeholder="Пароль"
+                    id='login-password-input' name='password' className="popup__input-item popup__input-item_el_password" type="password"
                     required minLength="2" maxLength="40" />
-                <span id='password-input-error' className="error"></span>
+                <span className="input-error">{errors && errors['password'] !== '' && errors['password']}</span>
 
             </PopupWithForm>
         </>
--- frontend/src/components/RegistrationPopupForm/RegistrationPopupForm.js
+++ frontend/src/components/RegistrationPopupForm/RegistrationPopupForm.js
@@ -1,47 +1,60 @@
 import PopupWithForm from '../PopupWithForm/PopupWithForm';
 import React from 'react';
+import { useFormWithValidation } from '../../utils/Validation/Validation'
+
+
 
 function RegistrationPopupForm({ isOpened, onClose }) {
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const [name, setName] = React.useState('');
+    // const [email, setEmail] = React.useState('');
+    // const [password, setPassword] = React.useState('');
+    // const [name, setName] = React.useState('');
 
-    function handleName(e) {
-        setName(e.target.value);
-    }
-    function handlePassword(e) {
-        setPassword(e.target.value);
-    }
-    function handleEmail(e) {
-        setEmail(e.target.value);
-    }
-    function handleSubmit(e) {
+    // function handleName(e) {
+    //     setName(e.target.value);
+    // }
+    // function handlePassword(e) {
+    //     setPassword(e.target.value);
+    // }
+    // function handleEmail(e) {
+    //     setEmail(e.target.value);
+    // }
+    // function handleSubmit(e) {
+    //     e.preventDefault();
+    //     console.log('SUBMIT')
+    //     // onRegistration({
+    //     //     profileNameInput: name,
+    //     //     profilePassword: password,
+    //     //     profileEmail: email,
+    //     // });
+
+    // }
+
+    const {value , handleChange, errors , isValid} = useFormWithValidation({});
+
+    const onSubmit = (e) => {
         e.preventDefault();
-        console.log('SUBMIT')
-        // onRegistration({
-        //     profileNameInput: name,
-        //     profilePassword: password,
-        //     profileEmail: email,
-        // });
-
+        if(isValid) {
+            console.log(value);
+        }
     }
+
     return (
         <>
-            <PopupWithForm onSubmit={handleSubmit} button='Сохранить' name='registration' title='Регистрация'
+            <PopupWithForm onSubmit={onSubmit} button='Сохранить' name='registration' title='Регистрация'
                 isOpened={isOpened} onClose={onClose}>
-                <input value={name || ''} onChange={handleName} placeholder="Имя" id="input-name"
-                    name='profileNameInput' className="popup__input-item popup__input-item_el_name" type="text"
+                <input  onChange={handleChange} placeholder="Имя" id="input-name"
+                    name='name' className="popup__input-item popup__input-item_el_name" type="text"
                     required minLength="2" maxLength="40" />
-                <span id='input-name-error' className="error"></span>
-                <input value={password || ''} onChange={handlePassword} placeholder="Пароль"
-                    id='registration-password-input' name='profilePasswordInput' className="popup__input-item popup__input-item_el_password" type="password"
-                    required minLength="2" maxLength="40" />
-                <span id='input-password-error' className="error"></span>
+                <span className="input-error">{errors && errors['name'] !== '' && errors['name']}</span>
+                <input  onChange={handleChange} placeholder="Пароль"
+                    id='registration-password-input' name='password' className="popup__input-item popup__input-item_el_password" type="password"
+                    required minLength="8" maxLength="16" />
+                <span className="input-error">{errors && errors['password'] !== '' && errors['password']}</span>
 
-                <input value={email || ''} onChange={handleEmail} placeholder="Почта"
-                    id='registration-email-input' name='profileEmailInput' className="popup__input-item popup__input-item_el_email" type="email"
-                    required minLength="2" maxLength="40" />
-                <span id='email-input-error' className="error"></span>
+                <input  onChange={handleChange} placeholder="Почта"
+                    id='registration-email-input' name='email' className="popup__input-item popup__input-item_el_email" type="email"
+                    required minLength="2" maxLength="40" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
+                <span className="input-error">{errors && errors['email'] !== '' && errors['email']}</span>
             </PopupWithForm>
         </>
     )
